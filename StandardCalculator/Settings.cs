ec36268bd36e9d3a39e5f29927bc706aacbc9523
Aastha Goyal <aastha2150@gmail.com>
2018-05-02 23:18:07 +1200

    Importing Project

---
+++ StandardCalculator/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StandardCalculator
+{
+    class Settings
+    {
+        public static string address = "118 Asquith avenue, Mount Albert";
+        public static string phoneNo = "5545783778789";
+    }
+}
